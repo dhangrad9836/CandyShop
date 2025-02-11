56dc09741e2a9abda1cc9f97616583442a50c7af
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-02-11 08:28:52 -0500

    First commit

--- CandyStore/Program.cs
+++ CandyStore/Program.cs
@@ -5,6 +5,11 @@ Console.WriteLine("Days since opening: " + 1);
 Console.WriteLine("Today's profit: " + 5.5m + "$");
 Console.WriteLine("Today's target achieved " + false);
 Console.WriteLine("-------------------------------------");
+Console.WriteLine("Choose one option: ");
+Console.WriteLine('V' + " to view products");
+Console.WriteLine('A' + " to Add products");
+Console.WriteLine('D' + " to Delete products");
+Console.WriteLine('U' + " to Update products");
 
 
 Console.ReadLine();
